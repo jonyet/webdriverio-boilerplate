655cdaf98b3f6044ebe42792f5734c3609aa7d2b
jterry <jterry@hugeinc.com>
2020-06-01 15:51:54 -0400

    updated to latest wdio dependencies and added a shitty pause to a test for now

--- examples/Amazon/Integration/Navigation/nav-expander.test.js
+++ examples/Amazon/Integration/Navigation/nav-expander.test.js
@@ -11,12 +11,16 @@ describe("Navigation : hamburger open/close", () => {
 
   it("clicks on the logo, expanding the nav", () => {
     Navigation.hamburger.click()
+    Navigation.hamburger.waitForDisplayed(undefined, true)
+    Navigation.closeExpandedNav.waitForDisplayed()
     expect(Navigation.expandedNav.isDisplayed()).to.be.equal(true)
   })
 
   it("closes again when clicked", () => {
     Navigation.closeExpandedNav.click()
     Navigation.closeExpandedNav.waitForDisplayed(undefined, true)
+    Navigation.hamburger.waitForDisplayed()
+    browser.pause(500)
     expect(Navigation.expandedNav.isDisplayed()).to.be.equal(false)
   })
 })
